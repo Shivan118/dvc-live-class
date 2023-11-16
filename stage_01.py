291fcdb11049fdbf815e28c3bd652d09cca3e1e5
shivan <kshivan848@gmail.com>
2023-11-16 20:09:52 +0530

    dvc file adde3dd

---
+++ stage_01.py
@@ -0,0 +1,4 @@
+
+
+with open("artifact.txt", "w") as f:
+    f.write("This is our first DVC Class")
---
+++ stage_02.py
@@ -0,0 +1,7 @@
+
+with open("artifact.txt", "r") as f:
+    text = f.read()
+
+print(text)
+print("Is is a end of our stages 2")
+print("tesxting our local file ssize")
